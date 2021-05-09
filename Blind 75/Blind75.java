db5a3b6e7c7ed1cb8447b5e45d8836fd3df9ea7e
Aayush Mathur <aamathur02@gmail.com>
2021-05-09 19:05:06 -0400

    worked some more on blind 75

--- Blind 75/Blind75.java
+++ Blind 75/Blind75.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Blind75 {
     
@@ -33,5 +36,30 @@ public class Blind75 {
         return null;
     }
 
-    
+    public int lengthOfLongestSubstringBF(String s) {
+
+        //edge case where string length is zero
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        Set<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+
+    }
+
+
 }
